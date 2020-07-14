f6be806fbcd5e45c829b8912f45aea7369e73cfc
samyakjain10 <sjsamyak2001@gmail.com>
2020-07-14 18:13:52 +0530

    additional files and changes for Heroku

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -126,8 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
